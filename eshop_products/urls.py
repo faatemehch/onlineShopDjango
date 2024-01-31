7b94cd597c109d7384f10aed9487ddd9c0669e40
Fch <faatemehchamani@Fchs-MacBook-Pro.local>
2024-01-31 09:10:56 +0330

    urls have been updated

--- eshop_products/urls.py
+++ eshop_products/urls.py
@@ -11,12 +11,12 @@ from .views import (
 )
 
 urlpatterns = [
-    path( 'products', ProductList.as_view(), name='list_products' ),
-    path( 'products-cheapest', CheapestProducts.as_view(), name='products-cheapest' ),
-    path( 'products-most-expensive', ExpensiveProducts.as_view(), name='products-most-expensive' ),
-    path( 'products-active', ActiveProducts.as_view(), name='products-active' ),
-    path( 'products-new', NewestProducts.as_view(), name='products-new' ),
-    path( 'products/<int:productId>', product_detail, name='product-detail' ),
-    path( 'products/search', SearchProductView.as_view(), name='products-search' ),
-    path( 'products/<category_name>', ProductListByCategory.as_view(), name='products-category' ),
+    path( 'products/', ProductList.as_view(), name='list_products' ),
+    path( 'products-cheapest/', CheapestProducts.as_view(), name='products-cheapest' ),
+    path( 'products-most-expensive/', ExpensiveProducts.as_view(), name='products-most-expensive' ),
+    path( 'products-active/', ActiveProducts.as_view(), name='products-active' ),
+    path( 'products-new/', NewestProducts.as_view(), name='products-new' ),
+    path( 'products/<int:productId>/', product_detail, name='product-detail' ),
+    path( 'products/search/', SearchProductView.as_view(), name='products-search' ),
+    path( 'products/<category_name>/', ProductListByCategory.as_view(), name='products-category' ),
 ]
